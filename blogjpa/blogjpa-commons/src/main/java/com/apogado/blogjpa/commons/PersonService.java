ccde33f04968bbaab1ac92a73967eeeb0ab1151a
admin <admin@Apogado_2>
2013-03-27 12:30:41 +0100

    blogjpa common

--- blogjpa/blogjpa-commons/src/main/java/com/apogado/blogjpa/commons/PersonService.java
+++ blogjpa/blogjpa-commons/src/main/java/com/apogado/blogjpa/commons/PersonService.java
@@ -6,8 +6,19 @@ package com.apogado.blogjpa.commons;
  * @author Gabriel Vince
  */
 public interface PersonService {
+    
+    /**
+     * persiste a new Person entity object
+     * @param p
+     * @return persistet object
+     */
     public Person createPerson(Person p);
     
+    /**
+     * return a Person entity object by id
+     * @param id
+     * @return 
+     */
     public Person getPerson(Integer id);
     
 }
